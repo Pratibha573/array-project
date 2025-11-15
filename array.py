ca83143abad2aa9ff1d07c74f9327b440db2101b
Pratibha573 <sypratibha63@gmail.com>
2025-11-15 21:23:39 +0530

    array.py

--- array.py
+++ array.py
@@ -1,7 +1,24 @@
-scores = list(map(int, input("Enter scores separated by space: ").split()))
+import sys
 
-total = sum(scores)
-avg = total / len(scores)
+if len(sys.argv) == 4:
+    english = int(sys.argv[1])
+    maths = int(sys.argv[2])
+    kannada = int(sys.argv[3])
+else:
+    english = 50
+    maths = 75
+    kannada = 100
 
-print("Sum =", total)
-print("Average =", avg)
+scores = [english, maths, kannada]
+
+
+score = sum(scores)
+avg = score / 3
+maximum = max(scores)
+minimum = min(scores)
+
+
+print("Total score:", score)
+print("Average:", avg)
+print("Maximum score:", maximum)
+print("Minimum score:", minimum)
